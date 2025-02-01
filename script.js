db6cc4fb703f3fdeeb3cbfefdc8e75f640de1c30
Vishal Singh <96664216+quantum0X@users.noreply.github.com>
2025-02-01 21:18:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// your code here
+const form = document.getElementById("form");
+const urlElement = document.getElementById("url");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); 
+
+  const name = document.getElementById("name").value;
+  const year = document.getElementById("year").value;
+
+  let queryString = ``;
+  if(name && year) queryString = `?name=${name}&year=${year}`
+  else if(name) queryString = `?name=${name}`
+  else if(year) queryString = `?year=${year}`	
+
+  urlElement.innerText = `https://localhost:8080/${queryString}`;
+});
